60d4025ee11fd10fe6ad784a6a291c970f87a3ff
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2025-06-05 14:49:24 +0530

    Setup MERN project with folder structure, Configure Tailwind in React and install require packges from frontend and backend

---
+++ Front-End/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello, Its Bug Tracker(Jira) Application</h1>
+    </>
+  )
+}
+
+export default App
