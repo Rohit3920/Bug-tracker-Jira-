5419ec3388b3cdaa2d26137caa85d1a2385dd533
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2025-06-19 10:39:16 +0530

    set vercel URL in both side(frontend & backend)

--- Front-End/src/getData/ConstantVal.jsx
+++ Front-End/src/getData/ConstantVal.jsx
@@ -1,4 +1,4 @@
-export const routeURL = 'bug-tracker-jira-application-qf6d.vercel.app'
+export const routeURL = 'bug-tracker-jira-application-backen.vercel.app'
 
 export const getAuthHeaders = () => {
     const token = localStorage.getItem('token');
